73631027b80907154a6680886ed7425c17d7fdec
mayankjain25 <jainmayank2509@gmail.com>
2021-09-02 21:45:49 +0530

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+
+
+function Header() {
+    return (
+        <div className="app">
+            <h1>This is a header</h1>
+        </div>
+    )
+}
+
+export default Header
+
